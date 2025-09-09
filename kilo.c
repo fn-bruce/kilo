0ddad51701824a8adbb8ccb1306606a4cfdf29a6
Bruce Nguyen <bruceleenguyen95@gmail.com>
2025-09-08 20:21:18 -0600

    feat: added tildes to left side of the screen

--- kilo.c
+++ kilo.c
@@ -74,8 +74,19 @@ void editor_process_keypress(void) {
 
 /*** output ***/
 
+void editor_draw_rows() {
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
 void editor_refresh_screen(void) {
   write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editor_draw_rows();
+
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
