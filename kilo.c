27b9017ca0962351e795af5beaa5b7de77b6bf5f
Bruce Nguyen <bruceleenguyen95@gmail.com>
2025-09-08 20:19:39 -0600

    feat: clear screen on exit

--- kilo.c
+++ kilo.c
@@ -18,6 +18,8 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
   perror(s);
   exit(1);
 }
@@ -63,6 +65,8 @@ void editor_process_keypress(void) {
 
   switch (c) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
