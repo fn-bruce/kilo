446cca4fb16e7c529ac2ed8e2909f5879393113d
Bruce Nguyen <bruceleenguyen95@gmail.com>
2025-09-09 09:40:09 -0600

    feat: added a tilde at the last line

--- kilo.c
+++ kilo.c
@@ -131,7 +131,11 @@ void editor_process_keypress(void) {
 void editor_draw_rows(void) {
   int y;
   for (y = 0; y < E.screen_rows; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    write(STDOUT_FILENO, "~", 3);
+
+    if (y < E.screen_rows - 1) {
+      write(STDOUT_FILENO, "\r\n", 2);
+    }
   }
 }
 
