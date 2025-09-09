d8a9ab66278b23dc4c3c3e4e0645e937d223ded9
Bruce Nguyen <bruceleenguyen95@gmail.com>
2025-09-09 06:53:04 -0600

    refactor: moved `editor_read_key` under `output` section

--- kilo.c
+++ kilo.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
 /*** defines ***/
 
@@ -64,6 +65,8 @@ char editor_read_key(void) {
   return c;
 }
 
+/*** output ***/
+
 void editor_process_keypress(void) {
   char c = editor_read_key();
 
@@ -75,8 +78,6 @@ void editor_process_keypress(void) {
       break;
   }
 }
-
-/*** output ***/
 
 void editor_draw_rows() {
   int y;
