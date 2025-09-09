ac985ea0ad6d66cf800d6a6732fb319b0d395178
Bruce Nguyen <bruceleenguyen95@gmail.com>
2025-09-08 20:09:15 -0600

    refactor: cleaned up keyboard input functionality

--- kilo.c
+++ kilo.c
@@ -47,24 +47,34 @@ void enable_raw_mode(void) {
   }
 }
 
+char editor_read_key(void) {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) {
+      die("read");
+    }
+  }
+  return c;
+}
+
+void editor_process_keypress(void) {
+  char c = editor_read_key();
+
+  switch (c) {
+    case CTRL_KEY('q'):
+      exit(0);
+      break;
+  }
+}
+
 /*** init ***/
 
 int main(void) {
   enable_raw_mode();
 
   while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-      die("read");
-    }
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == CTRL_KEY('q')) {
-      break;
-    }
+    editor_process_keypress();
   }
   return 0;
 }
